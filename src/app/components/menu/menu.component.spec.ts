74cd5aed491812d1f8a4a5b4a09d2c868be6f36d
evertonsilva <evertonsilva@luizalabs.com>
2021-12-20 22:50:40 -0300

    wip: added new unit tests and fixed others

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, PopoverController } from '@ionic/angular';
 
 import { MenuComponent } from './menu.component';
 
@@ -7,10 +7,22 @@ describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  const spyPopover = jasmine.createSpyObj('Popover', ['create', 'present', 'onDidDismiss', 'dismiss']);
+  spyPopover.onDidDismiss.and.returnValue({ data: { action: 'some action' } });
+  
+  const spyPopoverController = jasmine.createSpyObj('PopoverController', ['create', 'dismiss']);
+  spyPopoverController.create.and.callFake(() => {
+    return spyPopover;
+  });  
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: PopoverController, useValue: spyPopoverController }
+      ],
+
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
@@ -20,5 +32,13 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Component Behaviors', () => {
+    it('Should call popover dismiss', async() => {
+      await component.signout()
+      fixture.detectChanges();
+      expect(spyPopoverController.dismiss).toHaveBeenCalled();
+    });
   });
 });
